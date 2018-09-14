5be8e3b15e24df81b205525350fa6b86b93af5b4
Eric ATTOU <ricoattou@yahoo.fr>
2018-09-14 18:31:41 +0100

    Create multi module project. Closes #1

---
+++ eInsurance-web/src/main/java/net/itdeal/eInsurance/EInsuranceApplication.java
@@ -0,0 +1,12 @@
+package net.itdeal.eInsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EInsuranceApplication.class, args);
+	}
+}
